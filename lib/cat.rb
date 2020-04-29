e571bb579625aaef48275cc6f32b405c2da3ee09
Lucas Easley <lucaseasley11@gmail.com>
2020-04-29 00:28:11 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,21 +1,3 @@
-# class Cat
-#   attr_reader :name
-#   attr_accessor :owner, :mood
-#
-#     @@all = []
-#
-#   def initialize(name, owner)
-#     @name = name
-#     @owner = owner
-#     @mood = "nervous"
-#     @@all << self
-#   end
-#
-#   def self.all
-#     @@all
-#   end
-# end
-
 
 class Cat
   attr_reader :name
