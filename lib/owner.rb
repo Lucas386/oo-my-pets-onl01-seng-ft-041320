1fe767edf7bbc9bbb7355be3d58b615151edcf0c
Lucas Easley <lucaseasley11@gmail.com>
2020-04-29 00:44:44 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -30,11 +30,11 @@ class Owner
   def dogs
     Dog.all.select {|dog| dog.owner == self}
   end
-  
+
   def buy_cat(name)
     Cat.new(name, self)
   end
-  
+
   def buy_dog(name)
     Dog.new(name, self)
   end
